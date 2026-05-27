5eb52fc6cf0d62a519a2caa9f1333ada780dcc06
SeVu <seovulf@gmail.com>
2026-05-28 02:56:21 +0300

    Fix: Add secret restoration to cd.sh for CI/CD

--- cd.sh
+++ cd.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+mkdir -p android/fastlane
+echo "$GOOGLE_PLAY_JSON_KEY" > android/fastlane/play-store-key.json
+
+mkdir -p android/app
+echo "$KEYSTORE_BASE64" | base64 --decode > android/app/key.jks
+
 echo "flutter clean && flutter pub get"
 flutter clean && flutter pub get
 
